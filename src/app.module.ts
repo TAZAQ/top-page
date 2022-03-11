d870300ddf91a81d765d4c73cf9d173df5fa6d13
tazaq <tazaqsp@mail.ru>
2022-03-12 02:28:55 +0500

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { AuthModule } from './auth/auth.module'
-import { ProductModule } from './product/product.module'
-import { TopPageModule } from './top-page/top-page.module'
-import { ReviewModule } from './review/review.module'
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { AuthModule } from "./auth/auth.module"
+import { ProductModule } from "./product/product.module"
+import { TopPageModule } from "./top-page/top-page.module"
+import { ReviewModule } from "./review/review.module"
+import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [AuthModule, ProductModule, TopPageModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    ProductModule,
+    TopPageModule,
+    ReviewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
