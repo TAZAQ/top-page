f1d77668f2fab7d748a3e468bd4982675d30ad5a
tazaq <tazaqsp@mail.ru>
2022-03-13 04:08:16 +0500

    + Авторизация

--- src/auth/auth.consts.ts
+++ src/auth/auth.consts.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'Пользователь уже зарегистрирован'
+export const USER_NOT_FOUND = 'Пользователь не найден'
+export const WRONG_PASSWORD_ERROR = 'Неверный пароль'
